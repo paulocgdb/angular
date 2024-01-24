c0ca364fb80e5772bc9daa1d7f2bb280ba624f2a
Paulo Cesar <paulocesarti01@gmail.com>
2024-01-23 21:37:45 -0300

    feat: Basic Property Binding

--- main-app/src/app/success-alert/success-alert.component.ts
+++ main-app/src/app/success-alert/success-alert.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styleUrl: './success-alert.component.css'
 })
-export class SuccessAlertComponent {
+export class SuccessAlertComponent implements OnInit {
   serverStatus: string = 'Online';
   serverId: number = 10;
+  isNewServer: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+        this.isNewServer = true;
+      }, 2000
+    )
+  }
+
+  ngOnInit() {
+  }
 }
