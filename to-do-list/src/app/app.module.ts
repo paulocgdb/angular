f901d6f66e320375fa309999742dd9562c07a628
paulo.brito <paulocesarti01@gmail.com>
2024-05-16 23:49:00 -0300

    feature: Adding more components

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoFormComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
